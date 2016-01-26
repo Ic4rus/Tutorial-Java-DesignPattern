87f058307d2fed870c3e44dac354882788fa8ef5
Thang Hoang <thanght4391@gmail.com>
2016-01-26 21:35:30 +0700

    Facade

---
+++ src/com/icarus/tutorial/designpattern/facade/TestDiffcultProduct.java
@@ -0,0 +1,19 @@
+package com.icarus.tutorial.designpattern.facade;
+
+public class TestDiffcultProduct {
+	
+	public static void main(String[] args) {
+		
+		DifficultProduct difficultProduct = new DifficultProduct();
+		
+		difficultProduct.setFirstNameCharacter('p');
+		difficultProduct.setSecondNameCharacter('r');
+		difficultProduct.setThirdNameCharacter('i');
+		difficultProduct.setFourthNameCharacter('n');
+		difficultProduct.setFifthNameCharacter('t');
+		difficultProduct.setSixthNameCharacter('e');
+		difficultProduct.setSeventhNameCharacter('r');
+		
+	}
+
+}
